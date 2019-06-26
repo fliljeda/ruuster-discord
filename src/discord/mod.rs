380124c38493a7b28c597be42b044ede41464247
Fredrik Liljedahl <fredrik.liljedahl@hotmail.com>
2019-06-26 17:04:16 +0200

    Add basic message fetching

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -2,9 +2,11 @@ use super::config::Settings;
 use reqwest::{Client, Url};
 use serde::{Deserialize};
 use std::fmt;
+use std::{thread, time};
 
 const API_BASE_URL: &str = "https://discordapp.com/api/";
 
+#[derive(PartialEq)]
 enum ChannelType {
     Text = 0,
     DM = 1,
@@ -18,12 +20,20 @@ enum ChannelType {
 
 // Channel fields directly corresponds to a subset of the Discord api
 // channel response json
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Channel{
     #[serde(alias = "type")]
     ctype: u8,
     id: String,
     name: String,
+    last_message_id: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Message{
+    id: String,
+    channel_id: String,
+    content: String,
 }
 
 impl ChannelType {
@@ -57,6 +67,37 @@ impl Channel {
     fn get_channel_type(&self) -> ChannelType {
         ChannelType::from_u8(self.ctype)
     }
+
+    fn update_last_message(&mut self, msg: &Message){
+        self.last_message_id = Some(msg.id.clone());
+    }
+
+    fn get_new_messages(&mut self, client: &Client) -> Vec<Message> {
+        let url = Url::parse(&format!("{}channels/{}/messages", 
+                                     API_BASE_URL, 
+                                     self.id)).unwrap();
+        let mut req = client.get(url.clone());
+
+        req = req.query(&[("after", self.last_message_id.as_ref().unwrap())]);
+                                
+        let mut resp = req.send().unwrap();
+        let body = resp.text().unwrap();
+        let v : Vec<Message> = match serde_json::from_str(&body) {
+            Err(e) => {
+                println!("Something went wrong with deserializing json from url: {}", url);
+                panic!(e);
+            },
+            Ok(a) => a,
+        };
+
+        // Update the channel's last sent message id if new messages are found
+        // Returned messages starts from last sent message
+        if let Some(msg) = v.first().as_ref() {
+            self.update_last_message(msg);
+        }
+
+        v
+    }
 }
 impl fmt::Display for Channel {
     fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
@@ -69,6 +110,7 @@ pub fn test_connection(settings: &Settings) -> Result<(), reqwest::Error>{
     reqwest::get("https://httpbin.org/get")?;
     Ok(())
 }
+
 
 // TODO: Tests config values' validity for the discord API
 fn settings_valid(_settings: &Settings) -> bool {
@@ -113,16 +155,25 @@ fn get_channels(client: &Client, guild: &str) -> Vec<Channel> {
                                  API_BASE_URL, 
                                  guild)).unwrap();
 
-    println!("{:?}", url);
-    let mut resp = client.get(url).send().unwrap();
+    let mut resp = client.get(url.clone()).send().unwrap();
     let body = resp.text().unwrap();
-    //println!("{}", body);
     let v : Vec<Channel> = match serde_json::from_str(&body) {
-        Err(e) => panic!(e),
+        Err(e) => {
+            println!("Something went wrong with deserializing json from url: {}", url);
+            panic!(e);
+        },
         Ok(a) => a,
     };
     v
 }
+
+fn get_text_channels(client: &Client, guild: &str) -> Vec<Channel> {
+    let mut v = get_channels(client, guild);
+    v.retain(|c| c.get_channel_type() == ChannelType::Text);
+    v
+}
+
+
 
 
 // Starts the bot using Bot Token Authorization Header
@@ -134,7 +185,19 @@ pub fn start_bot(settings: &Settings) {
         Ok(c) => c,
         Err(e) => panic!(e),
     };
-    for x in get_channels(&client, &settings.guild) {
-        println!("{}", x);
+    let mut v = get_text_channels(&client, &settings.guild);
+
+    loop {
+
+        for x in &mut v {
+            let msgs = x.get_new_messages(&client);
+            for msg in msgs.iter().rev() {
+                println!("New message: {:?}", msg);
+            }
+        }
+
+        let ten_millis = time::Duration::from_millis(3000);
+        println!("Sleeping 3000 ms");
+        thread::sleep(ten_millis);
     }
 }
